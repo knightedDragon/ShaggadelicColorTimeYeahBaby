a8024ec510d57c5c2d8791cd44b1d2112a70c997
Aurora <kathryn.aurora.w@gmail.com>
2022-02-08 22:26:33 -0800

    Made new class, edited activity_main a bit

---
+++ app/src/main/java/com/example/customcoloringassignment/SecretView.java
@@ -0,0 +1,10 @@
+package com.example.customcoloringassignment;
+
+import android.content.Context;
+import android.view.SurfaceView;
+
+public class SecretView extends SurfaceView {
+    public SecretView(Context context) {
+        super(context);
+    }
+}
